eb3ad2f459bb3781fe30423ac2d3542b64e880a3
yasiru <yasiruinduwara56@gmail.com>
2024-11-22 23:25:56 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,13 @@
+# print("Hellow world")
+# print("python is fun")
+# print('we are programmers')
+# print(23455)
+# print("Hello \n world \n Hello")
+
+# print("Hello",end= " ")
+# print("World")
+
+# print("a","b","c","d", sep="#")
+
+age = 20
+print("i am" , age , "years old." )
